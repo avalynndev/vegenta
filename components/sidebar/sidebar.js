8538704782ea93602370868268bf4c33dd353b77
avalynndev <avalynndev@gmail.com>
2022-12-09 19:42:41 +0530

    test 103

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -33,8 +33,8 @@ const Sidebar = ({ visit }) => {
                 height={40}
                 src={
                   theme.theme == "dark"
-                    ? "/animexlogodarksidebar.svg"
-                    : "/animexlogolightsidebar.svg"
+                    ? "/animexlogodarksidebar.png"
+                    : "/animexlogolightsidebar.png"
                 }
               />
             </div>
